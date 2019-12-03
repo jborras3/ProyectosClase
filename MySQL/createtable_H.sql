07943fffc3e3df1048af2a7f3bc53855087cf6a0
jborras3 <jborras3@nigul.cide.es>
2019-12-03 20:43:25 +0100

    2n

---
+++ MySQL/createtable_H.sql
@@ -0,0 +1,9 @@
+create table p2.h (
+codi_hospital int(10) NOT NULL,
+dni varchar(9) character set utf8 not null,
+apellidos varchar(30) character set utf8 not null,
+funcion varchar(9) character set utf8 not null,
+salario int(5) not null,
+localidad varchar(15) character set utf8 not null,
+PRIMARY KEY (dni)
+);
