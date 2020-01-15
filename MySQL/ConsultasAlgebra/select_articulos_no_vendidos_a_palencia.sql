192c0e63c984b4f5663d5e6d7b44603644fd2cbe
jborras3 <jborras3@nigul.cide.es>
2020-01-15 16:34:45 +0100

    15/01/2020

---
+++ MySQL/ConsultasAlgebra/select_articulos_no_vendidos_a_palencia.sql
@@ -0,0 +1,3 @@
+select Codi_Producte from exercicis_algebra.venda 
+inner join exercicis_algebra.clients 
+where Poblacio <> 'Palència';
---
+++ MySQL/ConsultasAlgebra/select_articulos_vendidos_a_palenciai_vallalodid.sql
@@ -0,0 +1,3 @@
+select Descripcio from exercicis_algebra.venda 
+inner join exercicis_algebra.clients inner join exercicis_algebra.producte 
+where Poblacio = 'Palència' or Poblacio = 'Valladolid';
